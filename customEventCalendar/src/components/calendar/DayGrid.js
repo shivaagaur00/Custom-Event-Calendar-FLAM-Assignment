595f46c41df54df10f707e1a4bb2611a0daae1f7
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-07-21 00:57:38 +0530

    feat: create Component DayGrid

---
+++ customEventCalendar/src/components/calendar/DayGrid.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { 
+  startOfMonth, 
+  endOfMonth, 
+  startOfWeek, 
+  endOfWeek, 
+  eachDayOfInterval,
+  isSameMonth, 
+  isSameDay,
+} from 'date-fns';
+import DayCell from './DayCell';
+
+export default function DayGrid() {
+  const currentDate = useSelector(state => new Date(state.calendar.currentDate));
+  
+  const monthStart = startOfMonth(currentDate);
+  const monthEnd = endOfMonth(currentDate);
+  const startDate = startOfWeek(monthStart);
+  const endDate = endOfWeek(monthEnd);
+  const days = eachDayOfInterval({ start: startDate, end: endDate });
+
+  return (
+    <div className="grid grid-cols-7">
+      {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(day => (
+        <div 
+          key={day} 
+          className="py-3 text-center text-sm font-medium text-gray-500 uppercase bg-gray-50"
+        >
+          {day}
+        </div>
+      ))}
+      
+      {/* Calendar Days */}
+      {days.map(day => (
+        <DayCell 
+          key={day.toString()}
+          day={day}
+          isCurrentMonth={isSameMonth(day, monthStart)}
+          isToday={isSameDay(day, new Date())}
+        />
+      ))}
+    </div>
+  );
+}
